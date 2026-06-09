193cf7e8e57c8ccbcfdb435de9a0b990585a83a1
TomBlo19 <bolotomas3@gmail.com>
2026-06-09 12:46:50 -0300

    Agregar pruebas unitarias de funcionalidades criticas

--- tests/Controllers/contratosCriticosTest.php
+++ tests/Controllers/contratosCriticosTest.php
@@ -1,1 +1,70 @@
+<?php
 
+namespace Tests\Controllers;
+
+use PHPUnit\Framework\TestCase;
+use App\Controllers\Publicacion;
+use App\Controllers\Valoracion;
+
+class contratosCriticosTest extends TestCase
+{
+    public function testPublicarResena()
+    {
+        $publicacion = new Publicacion();
+
+        $resultado = $publicacion->publicarResena(
+            1, 
+            5, 
+            'Reseña creada desde PHPUnit'
+        );
+
+        $this->assertTrue($resultado);
+    }
+
+   public function testValorarReceta()
+{
+    $valoracion = new Valoracion();
+
+    $resultado = $valoracion->valorarPublicacion(
+        'receta', // Usa EstrategiaReceta
+        1,         // Usuario que vota
+        5,         // ID de la receta
+        1          // Like
+    );
+
+    $this->assertNotNull($resultado);
+}
+
+
+public function testPublicarReceta()
+{
+    session()->set('id_usuario', 1);
+
+    $imagen = $this->createMock(
+        \CodeIgniter\HTTP\Files\UploadedFile::class
+    );
+
+    $imagen->method('isValid')
+            ->willReturn(true);
+
+    $imagen->method('getRandomName')
+            ->willReturn('prueba.jpg');
+
+    $imagen->method('move')
+            ->willReturn(true);
+
+    $publicacion = new Publicacion();
+
+    $resultado = $publicacion->publicarReceta(
+        'Receta PHPUnit',
+        'Descripción suficientemente larga',
+        'harina, huevo',
+        1,
+        $imagen
+    );
+
+    $this->assertIsInt($resultado);
+}
+
+
+    }
